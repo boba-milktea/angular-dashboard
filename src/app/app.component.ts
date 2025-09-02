130c288caa9e0a2b6d161712b46b4127f93056cf
boba-milktea <catherine.idv@gmail.com>
2025-09-02 17:06:32 +0200

    the base of the app

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { ServerStatusComponent } from './server-status/server-status.component';
+import { TrafficComponent } from './traffic/traffic.component';
+import { TicketsComponent } from './tickets/tickets.component';
+import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    ServerStatusComponent,
+    TrafficComponent,
+    TicketsComponent,
+    DashboardItemComponent,
+  ],
+  templateUrl: './app.component.html',
+})
+export class AppComponent {}
